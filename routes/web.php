1538e054433e23badecf2f7c63166275a258606d
Chuma <hamyychuma.iron26@gmail.com>
2021-11-26 20:09:10 +0300

    about

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    return view('about');
 });
