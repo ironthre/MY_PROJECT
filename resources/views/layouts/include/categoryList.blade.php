84f401f0b4cf89f352dee6d7eedebb8aafd82f19
Chuma <hamyychuma.iron26@gmail.com>
2022-01-09 00:36:24 +0300

    USER_SIDE

--- resources/views/layouts/include/categoryList.blade.php
+++ resources/views/layouts/include/categoryList.blade.php
@@ -1,34 +1,14 @@
-<div class="py-3">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-6 col-md-4 col-lg-3 bg-primary">
-                <h4>Mobile Phone</h4>
-            </div>
-            <div class="col-sm-6 col-md-4 col-lg-3 bg-success">
-                <h4>Phone Accessories</h4>
-            </div>
-            <div class="col-sm-6 col-md-4 col-lg-3 bg-danger">
-                <h4>Music System</h4>
-
-            </div>
-            <div class="col-sm-6 col-md-4 col-lg-3 bg-info">
-                <h4>Home Decoration</h4>
-
-            </div>
-            <div class="col-sm-6 col-md-4 col-lg-3 bg-warning">
-                <h4>Refrigerator</h4>
-            </div>
-            <div class="col-sm-6 col-md-4 col-lg-3 bg-info">
-                <h4>Women's Bag</h4>
-            </div>
-             <div class="col-sm-6 col-md-4 col-lg-3 bg-success">
-                <h4>Kanga na Vitenge</h4>
-
-            </div>
-            <div class="col-sm-6 col-md-4 col-lg-3 bg-danger">
-                <h4>Mashuka na Vikoi</h4>
-
-            </div>
+<div class="container">
+    <div class="row">
+        <div class="card-group">
+            @foreach ($trending_category as $trend )
+                <div class="card">
+                    <img src="{{asset('assets/uploads/category/'.$trend->image)}}" alt="Category Image" class="card-img-top">
+                    <div class="card-body">
+                        <h6>{{$trend->name}}</h6>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
 </div>
